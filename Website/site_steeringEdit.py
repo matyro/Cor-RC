2c0f424410191da635db42aea2516b1aeed9c184
Matyro <dominik.baack@udo.edu>
2016-07-23 02:35:45 +0200

    Adde basic d&d, more std design

--- Website/site_steeringEdit.py
+++ Website/site_steeringEdit.py
@@ -7,10 +7,12 @@ import tornado.web
 class SteeringCardEditHandler(BaseHandler):
     @tornado.web.authenticated  
     def get(self):  
-        if self.get_arguments('id'):
-            tmp = self.database.query(Steering_Card).filter(Steering_Card.id==self.get_arguments('id')). \
-                                    filter( _or(Steering_Card.privat==False), _and(Steering_Card.privat==True, Steering_Card.user_id==self.current_user.id)).all()
-        self.render('edit_steeringcard.html', steering=tmp)
+        #if self.get_arguments('id'):
+        #    tmp = self.database.query(Steering_Card).filter(Steering_Card.id==self.get_arguments('id')). \
+        #                            filter( _or(Steering_Card.privat==False), _and(Steering_Card.privat==True, Steering_Card.user_id==self.current_user.id)).all()
+        #    self.render('edit_steeringcard.html', steering=tmp)
+            
+        self.render('edit_steeringcard.html', steering=None)
         
     def post(self):
         return

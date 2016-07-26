a6136bf0e07ef1a68c8e4a342579621bba41cb54
Matyro <dominik.baack@udo.edu>
2016-07-27 00:11:25 +0200

    Tooltip support

--- Website/site_steeringEdit.py
+++ Website/site_steeringEdit.py
@@ -4,6 +4,33 @@ from SQL.table_steeringcard import Steering_Card
 
 import tornado.web
 
+
+class Setting(object):
+    def __init__(self):
+        self.name = 'empty name'
+        self.content = '<a> content </a>'
+        self.desc = 'Description_of this setting'
+
+
+import xml.etree.ElementTree as etree 
+def createSettings():
+    tree = etree.parse('steering_75xx.xml')
+    root = tree.getroot()
+    for itr in root:
+        #print(itr)
+        #print(counter)
+        counter += 1    
+        
+    print(counter)
+    
+    for el in root.getchildren():
+        name = el.find('name').text
+        for x in el.getchildren():
+            print(x)
+
+    
+
+
 class SteeringCardEditHandler(BaseHandler):
     @tornado.web.authenticated  
     def get(self):  
@@ -12,7 +39,10 @@ class SteeringCardEditHandler(BaseHandler):
         #                            filter( _or(Steering_Card.privat==False), _and(Steering_Card.privat==True, Steering_Card.user_id==self.current_user.id)).all()
         #    self.render('edit_steeringcard.html', steering=tmp)
             
-        self.render('edit_steeringcard.html', steering=None)
+        settings = []
+        settings.append( Setting() )
+        
+        self.render('edit_steeringcard.html', steering=settings)
         
     def post(self):
         return

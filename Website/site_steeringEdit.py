7e349c76c49f3ae7af3cba6d4008955d8c277406
Matyro <dominik.baack@udo.edu>
2016-07-30 01:49:51 +0200

    steering edit works fine now

--- Website/site_steeringEdit.py
+++ Website/site_steeringEdit.py
@@ -5,6 +5,11 @@ from SQL.table_steeringcard import Steering_Card
 import tornado.web
 
 
+def xstr(s):
+    if s is None:
+        return ''
+    return str(s)
+
 class Setting(object):
     def __init__(self, name, desc, content):
         self.name = name
@@ -23,7 +28,37 @@ def createSettings():
         desc = el.find('desc').text
         content = ''
         for x in el.find('parameter'):
-           content = content + ', ' + str(x.find('name').text)
+            if x.find("format").text == 'I':
+                content = content + '<div class=\"form-group\"><div class=\"form-inline\"><label id=\"tt\" title=\"' + xstr(x.find('desc').text)  + '\">'          
+                content = content + xstr(x.find('name').text) + ':</label>'
+                content = content + '<input type=\"number\" step=\"any\" class=\"input-sm \" name=\"' + xstr(x.find('name').text) + '\"'
+                ##content = content + ' id=\"' + xstr(x.find('name').text) + '\"'
+                content = content + '\" value=\"' + xstr(x.find('default').text) + '\"'
+                content = content +  '/></div></div> <!-- I -->'
+                    
+            elif x.find("format").text == 'F':
+                content = content + '<div class=\"form-group\"><div class=\"form-inline\"><label id=\"tt\" title=\"' + xstr(x.find('desc').text)  + '\">'          
+                content = content + xstr(x.find('name').text) + ':</label>'
+                content = content + '<input type=\"number\" step=\"any\" class=\"input-sm \" name=\"' + xstr(x.find('name').text) + '\"'
+                content = content + 'value=\"' + xstr(x.find('default').text) + '\"' 
+                content = content + '/></div></div> <!-- F -->'
+                    
+            elif x.find("format").text == 'L':
+                content = content + '<div class=\"form-group\"><div class=\"form-inline\"><label id=\"tt\" title=\"' + xstr(x.find('desc').text)  + '\">'          
+                content = content + xstr(x.find('name').text) + ':</label>'
+                content = content + '<input type=\"checkbox\" class=\"input-sm \" name=\"' + xstr(x.find('name').text) + '\"'
+                content = content + 'value=\"' + xstr(x.find('default').text) + '\"' 
+                content = content + '/></div></div> <!-- L -->'                
+                   
+            else: 
+                content = content + '<div class=\"form-group\"><div class=\"form-inline\"><label id=\"tt\" title=\"' + xstr(x.find('desc').text)  + '\">'          
+                content = content + xstr(x.find('name').text) + ':</label>'
+                content = content + '<input maxlength=\"256\" type=\"text\" class=\"input-sm \" name=\"' + xstr(x.find('name').text) + '\"'
+                content = content + 'value=\"' + xstr(x.find('default').text) + '\"' 
+                content = content + '/></div></div> <!-- ELSE -->'
+                
+            content = content + '\n'
+            
 
         list.append(Setting(name,desc,content))
     
@@ -41,6 +76,7 @@ class SteeringCardEditHandler(BaseHandler):
         self.render('edit_steeringcard.html', steering=createSettings())
         
     def post(self):
+        print(self.request.arguments)
         return
 
 
